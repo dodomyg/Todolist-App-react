d2a8ea6e776a8793a08517c87fe2e65c61071907
dodomyg <sharvil.goverdhan@gmail.com>
2023-04-06 22:27:15 +0530

    todolist

---
+++ src/Components/Box.jsx
@@ -0,0 +1,63 @@
+import React,{useState} from 'react'
+import "./box.css"
+
+
+
+
+
+
+function Box() {
+
+    const [text,setText]=useState('');
+
+    const[item,setItem]=useState([]);
+
+    function gg(event){
+
+        const update=event.target.value
+        setText(update);
+
+    }
+
+    function addItem(){
+       setItem((prevItem)=>{
+        return [...prevItem,text]
+       })
+       setText('')
+    }
+
+   
+
+
+
+
+
+
+
+  return (
+    <div className='box'>
+        <div className='heading'>
+            <h1>To-Do-List</h1>
+        </div>
+        <div className='form'>
+            <input onChange={gg} type='text' value={text}/>
+            <button
+
+            onClick={addItem}
+            
+            >Add</button>
+        </div>
+        <div>
+            <ul>
+                {item.map((todoItem)=>{
+                    return <li>{todoItem}</li>
+
+                })}
+            </ul>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Box
---
+++ src/Components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './container.css'
+import Box from './Box'
+
+function Container() {
+  return (
+    <div className='container'>
+      <Box />
+    </div>
+  )
+}
+
+export default Container
